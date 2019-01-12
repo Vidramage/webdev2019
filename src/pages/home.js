ed3a3d6f1884b3c8328917d978e2db035aa9e865
chris <chrshawkes@yahoo.com>
2019-01-12 17:13:13 -0500

    added state to a component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/header';
 import Grid from '../components/grid';
+import Button from '../components/button';
 
 export default class Home extends React.Component {
     render() {
         let myVar = "holy crap";
         return (
             <div>
+                <Button></Button>
                 <Header title={this.props.title ? this.props.title : "no title given"}></Header>
                 <Grid data={[<div><h1>holy crap html header</h1></div>, "two", "three"]} />
             </div>
