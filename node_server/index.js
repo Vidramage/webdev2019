c929930e24a2eaae0978c7f5b24dbb62663db28c
chris <chrshawkes@yahoo.com>
2019-01-20 21:32:28 -0500

    changed port

--- node_server/index.js
+++ node_server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 9081
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
 app.use('/static', express.static('../dist'))
