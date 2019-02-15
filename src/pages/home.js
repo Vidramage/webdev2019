cc03b42615731b79fe82072fae525ddf40fbfae6
Michael M <mikem255@gmail.com>
2019-02-14 19:16:27 -0800

    how react components communicate with each other

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,7 +8,7 @@ export default class Home extends React.Component {
         return (
             <div>
                 <Header title="Michael Mahrt Portfolio"></Header>
-                <Grid dynamicTitle={"testing " + myVar} />
+                <Grid data={[<div><h1>holy crap html header</h1></div>, "two", "three"]} />
             </div>
         )
     }
